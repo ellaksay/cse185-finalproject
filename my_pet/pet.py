ba8b462390445a32ea2471400e6a3ccdcb4e813a
b1sanchez <96998684+b1sanchez@users.noreply.github.com>
2023-05-21 14:30:03 -0700

    Update pet.py

--- my_pet/pet.py
+++ my_pet/pet.py
@@ -1,25 +1,36 @@
+#!/usr/bin/env python
+
+"""
+Command-Line script to perform paired-end sequence trimming of FASTQ files
+
+Similar to sickle pe
+"""
+
 import argparse 
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog = "pet"
+    parser = argparse.ArgumentParser (
+        prog = "pet", 
         description = "Command-line script to trim paired-end reads"
     )
 
-    #Input
+    # Input
     parser.add_argument("se",help = "Single End",\
         type=str)
     
     # Output
     parser.add_argument("-o" "--out",help="Write output file." \
         "Default: stdout",metavar="FILE",type=str,required = False)
-
+    
+    # Other options
     parser.add_argument("-f" "--forward",help="Forward read." \
         "Default: stdout",metavar="FILE",type=str,required = False)
     parser.add_argument("-r" "--reverse",help="Reverse read." \
         "Default: stdout",metavar="FILE",type=str,required = False)
     
      # Parse args
-     args = parser.parse_args()
+    args = parser.parse_args()
     
-# Running: ./pet.py --help
+if __name__ == "__main__":
+    main()
+# Running: /usr/local/bin/python3 pet.py --help
