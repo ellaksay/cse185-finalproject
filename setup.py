cd2137bd334c0b04d4eecaab3cfc5564017e4598
b1sanchez <96998684+b1sanchez@users.noreply.github.com>
2023-05-21 15:14:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-
+setup(
+  name='PET',
+  version=VERSION, 
+  Description='CSE 185 PET Project', 
+  authors='Brianna Sanchez, Ella Say, Anu Selvaraj',
+  pacakages=find_packages(),
+  entry_points={
+    "console_scripts": {
+        "pet=pet.pet:main"
+      ],
+  },
+)
