a726ba61fd37c7d29943b10a501470466c105249
b1sanchez <96998684+b1sanchez@users.noreply.github.com>
2023-06-04 14:41:11 -0700

    Update fastq.py

--- fqutil/fastq.py
+++ fqutil/fastq.py
@@ -1,5 +1,5 @@
 """
-Utilities for mypileup
+Utilities for pet.py
 """
 from termios import TIOCPKT_DOSTOP
 import os
@@ -197,54 +197,3 @@ class Fastq:
             idx[read.unique_id()] = (read.seq, read.quals)
 
         return idx
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-def ERROR(msg):
-	"""
-	Print an error message and die
-
-	Parameters
-	----------
-	msg : str
-	   Error message to print
-	"""
-	sys.stderr.write(bcolors.FAIL + "[ERROR]: " + bcolors.ENDC + "{msg}\n".format(msg=msg) )
-	sys.exit(1)
-
-def GetQual(qual):
-    """
-    Convert qual score to ASCII
-    Use encoding chr(qual+33)
-    """
-    #Todo
-
-def TrimLonger(forward, reverse):
-    """
-    If forward FQ is longer than reverse FQ then trim
-    to be the same length 
-
-    Parameters
-    ----------
-    forward: fastq
-    reverse: fastq
-    """
-
-def ReadFastq(file):
-    """
-    Return just the important lines of the fastq file
-    
-    """
-
-    
-
-
