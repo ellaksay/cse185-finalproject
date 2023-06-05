ae5bbda081fef3e1db038eccbb47999c1002cc69
b1sanchez <96998684+b1sanchez@users.noreply.github.com>
2023-06-04 19:20:16 -0700

    Update set.py

--- set.py
+++ set.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-
-# TEST COMMAND
-# python set.py -o output.fastq -f ~/example_files/test.f.fastq \
-# -q 20 -m 30 -M 999999 pe
 """
 Command-Line script to perform single-end sequence trimming of FASTQ files
 
@@ -19,7 +15,6 @@ from itertools import islice
 current_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(current_dir)
 
-
 def main():
     parser = argparse.ArgumentParser (
         prog = "set", 
@@ -29,7 +24,7 @@ def main():
     # Input
     parser.add_argument("se", help = "Single End", \
         type=str)
-
+    
     # Output
     parser.add_argument("-o", "--out",help="Write output file. " \
         "Default: stdout", metavar="FILE", type=str, required=False)
@@ -40,92 +35,94 @@ def main():
     parser.add_argument("-t", "--qual-type", help="Type of quality values: " \
             "solexa (CASAVA < 1.3), illumina (CASAVA 1.3 to 1.7), sanger (CASAVA >= 1.8)", \
             type=str, metavar="QUALITY TYPE", required=False)
-    parser.add_argument('-q', '--min-qual', default=30, nargs=1, type=int, \
-            metavar="INTEGER",help='Minimum phred score. Disabled by setting it to -10.', required=False)
-    parser.add_argument('-m', '--min-length', default=30, nargs=1, type=int, \
-            metavar="INTEGER",help='Minimum read length after trimming.',required=False)
-    parser.add_argument('-M', '--max-length', default=99999999, nargs=1, type=int,\
-            metavar="INTEGER",help='Maxmimum read length after trimming.',required=False)
+    parser.add_argument('-q', '--min-qual', default=30, type=int, \
+            help='Minimum phred score. Disabled by setting it to -10.', required=False)
     
      # Parse args
     args = parser.parse_args()
 
-    # Needs reconstruction: Load fastq
+    # Load fastq
     if args.input is None:
         f_in_fastq = None
     else:
         f_in_fastq = args.input
 
-    # Needs reconstruction: Load qual_type
+    # Load qual_type
     if args.qual_type is None:
         quality_type = None
     else:
         quality_type = args.qual_type
 
-    # Needs reconstruction: Set up forward output file
+    # Set up output file
     if args.out is None:
         f_out_fastq = sys.stdout
     else: 
         f_out_fastq = args.out
-
+        
+    # Load phred score
     if args.min_qual is None:
-        min_qual = sys.stdout
+        min_qual = 30
     else:
         min_qual = args.min_qual
-    
-    if args.min_length is None:
-        min_len = sys.stdout
-    else:
-        min_len = args.min_length
-
-    if args.max_length is None:
-        max_len = sys.stdout
-    else:
-        max_len = args.max_length
-
+        
     #Testing 
-    print("f:", f_in_fastq)
-    print("t:", quality_type)
-    print("o:", f_out_fastq)
-    print("q:", min_qual)
-    print("m:", min_len)
-    print("M:", max_len)
-
+    print("Input fastq file(f):", f_in_fastq)
+    print("Quality type(t):", quality_type)
+    print("Output sequence(o):", f_out_fastq)
+    print("Minimum quality score(q):", min_qual)
+    
     def convert_qual_chars_to_scores(qual_chars):
         return [ord(char) - 33 for char in qual_chars]
 
-    def ReadFastq(threshold, file):
-        with open(file, 'r') as f:
+    def filterFastq(threshold, file):
+        with open(file, 'r') as input_file:
             sequences = []
+            qualities = []
             while True:
-                fastq_line = list(islice(f, 4))
-                if not fastq_line:
+                fastq_section = list(islice(input_file, 4))
+                if not fastq_section or len(fastq_section) < 4:
                     break
 
                 # Sequence and quality string
-                sequence = fastq_line[1].strip()
-                quality_string = fastq_line[3].strip()
+                sequence = fastq_section[1].strip()
+                quality_string = fastq_section[3].strip()
                 
                 # Convert quality string to scores
                 quality_scores = convert_qual_chars_to_scores(quality_string)
 
                 # Filter sequence by quality score threshold
                 filtered_sequence = ""
+                filtered_quality = ""
                 for i in range(len(sequence)):
                     if quality_scores[i] > threshold:
                         filtered_sequence += sequence[i]
+                        filtered_quality += quality_string[i]
                     else:
                         break
                         
                 # Add filtered sequence to list
                 sequences.append(filtered_sequence)
+                qualities.append(filtered_quality)
    
-        return '\n'.join(sequences)
-     
-    filtered_sequences = ReadFastq(min_qual, f_in_fastq)  # replace with threshold
-    print(filtered_sequences)
-   
+        return sequences, qualities
+                    
+    def outputFile(oFile, iFile, sequences, qualities):
+        with open(oFile, 'w') as output_file, open(iFile, 'r') as input_file:
+            for sequence, quality in zip(sequences, qualities):
+                fastq_line = list(islice(input_file, 4))
+                if not fastq_line:
+                    break
+                    
+                # write header and '+'
+                output_file.write(fastq_line[0])  # header
+                output_file.write(sequence + '\n')  # sequence
+                output_file.write(fastq_line[2])  # '+'
+                output_file.write(quality + '\n')  # quality
+
+    filtered_sequences, filtered_qualities = filterFastq(min_qual, f_in_fastq)
+    results = outputFile(f_out_fastq, f_in_fastq, filtered_sequences, filtered_qualities) 
+    
 if __name__ == "__main__":
     main()
 # Print the help message: python3 set.py --help
-# Print the tester: python3 set.py se -f ~/cse185-finalproject/example_files/test.f.fastq -t sanger -o ~/cse185-finalproject/results/test.f_trimmed.fastq
+# Print the tester: python3 set.py se -f ~/cse185-finalproject/test/test.fastq -t sanger -o ~/cse185-finalproject/results/test_trimmed.fastq -q 20
