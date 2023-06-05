6aaf65ce3452d96dd1a8ab600c22e04c8b29f6b8
b1sanchez <96998684+b1sanchez@users.noreply.github.com>
2023-06-04 19:58:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,6 @@ MAJ = 0
 MIN = 0
 REV = 0
 VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
-with open(os.path.join(curdir, 'my_pet/version.py'), 'w') as fout:
-        fout.write(
-            "\n".join(["",
-                       "# THIS FILE IS GENERATED FROM SETUP.PY",
-                       "version = '{version}'",
-                       "__version__ = version"]).format(version=VERSION)
-        )
-
 
 setup(
     name='SET',
